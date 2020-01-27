8e78df4d3db0fa308b427ed6be4d90ba5c61cd5b
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-27 05:37:39 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(array)
   if block_given?
     i = 0 
     while i < array.length do 
-      first_names << yield(array[i]).split(" ")
+      first_names << yield(array[i])
       i += 1 
     end
   else
